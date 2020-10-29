4a78f4c3a99974d2fb8f6b0612bc4266b3d8b29d
ivanv <ivan.vili91@gmail.com>
2020-10-29 10:47:38 +0100

    Adding NavigationBar to Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,27 +1,23 @@
-import React from 'react' 
+import React from 'react'
 import styles from './style.module.css'
-
-const navTabs = ['Home', 'Accommodation', 'Photo Gallery', 'Contact']
  
-  const Footer = () => (
-    <footer className={styles.footer}>
-        <ul className={styles.address}>
-            <li className={styles.title}>
-                VILLA OLIVA VERDE
-            </li>
-            <li>Štrped 24</li>
-            <li>521000 Vinkuran</li>
-            <li className={styles.phone}>
-                +385 99 11223344
-            </li>
-            <li>example@email.com</li>
-        </ul>
-        <ul className={styles.navigation}>
-            {navTabs.map(tab =>
-              <li>{tab}</li>)
-            }
-        </ul>
-    </footer>
-  )
+import NavigationBar from '../NavigationBar'
  
-  export default Footer
+const Footer = () => (
+  <footer className={styles.footer}>
+      <ul className={styles.address}>
+          <li className={styles.title}>
+              VILA OLIVA VERDE
+          </li>
+          <li>Štrped 24</li>
+          <li>521000 Vinkuran</li>
+          <li className={styles.phone}>
+              +385 99 11223344
+          </li>
+          <li>example@email.com</li>
+      </ul>
+      <NavigationBar useThisStyle = "footer"/>
+  </footer>
+)
+ 
+export default Footer
--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,16 +1,14 @@
 import React from 'react'
- 
+import { navs as navTabs } from '../../constants/const'
 import styles from './style.module.css'
-
-const navTabs = ['Home', 'Accommodation', 'Photo Gallery', 'Contact']
-
-//na njegovim uputama su falile normalne zagrade oko {activeTab}
-const NavigationBar = ({activeTab}) => (
-  <nav className={styles.navigationBar}>
-    {navTabs.map(tab => <li className={tab === activeTab ? styles.active : ''}>
-      {tab}</li>
-    )}
+ 
+//nezz zasto ovo ne radi "navigationBar is not defined"
+const NavigationBar = ({ activeTab, useThisStyle }) => (
+  <nav className={styles[useThisStyle || 'navigationBar']}>
+      {navTabs.map(tab => <li className={tab=== activeTab ? styles.active : ''}>
+          {tab}</li>
+      )}
   </nav>
 )
-
+ 
 export default NavigationBar
