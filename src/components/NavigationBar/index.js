59894f2736c772f7b3a644ce183f750979ac1915
ivanv <ivan.vili91@gmail.com>
2020-10-31 23:46:11 +0100

    contact page init

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { navs as navTabs } from '../../constants/const'
 import styles from './style.module.css'
- 
-//nezz zasto ovo ne radi "navigationBar is not defined"
+import { Link } from 'gatsby'
+
 const NavigationBar = ({ activeTab, useThisStyle }) => (
-  <nav className={styles[useThisStyle || 'navigationBar']}>
-      {navTabs.map(tab => <li className={tab=== activeTab ? styles.active : ''}>
-          {tab}</li>
-      )}
-  </nav>
+    <nav className={styles[useThisStyle || 'navigationBar']}>
+        {navTabs.map(({tab, to}) => (
+          <Link to={to} >
+              <li className={tab === activeTab ? styles.active : ''}>{tab}</li>
+          </Link>)
+        )}
+    </nav>
 )
- 
+
 export default NavigationBar
